0e2dae00b84cfae75eca2f54b4346dcb79de38dc
gdgeek <dirui1@yeah.net>
2024-11-14 02:14:28 +0800

    no message

--- src/localization/index.js
+++ src/localization/index.js
@@ -347,8 +347,8 @@ Blockly.Msg.SIGNAL_OUTPUT_SIGNAL = {
 };
 Blockly.Msg.SIGNAL_INPUT_SIGNAL = {
   zh: "接收信号! %1 %2 %3",
-  en: "Receive signal %1 %2 %3",
-  ja: "信号 %1 %2 %3 を受信する",
+  en: "Receive! signal %1 %2 %3",
+  ja: "信号! %1 %2 %3 を受信する",
 };
 Blockly.Msg.SIGNAL_OUTPUT_SIGNAL_WITH_PARAMETER = {
   zh: "触发信号 %1 参数 %2",
