3b96f2d8f864e9649cdd4342200aaa59d5e634e4
huyuelong <949910047@qq.com>
2025-03-25 11:03:57 +0800

    feat:

--- src/blocks/trigger/action_trigger.js
+++ src/blocks/trigger/action_trigger.js
@@ -22,12 +22,17 @@ const block = {
               console.error("resource", resource);
               const action = resource.action;
               action.forEach(({ name, uuid, type }) => {
-                if (type === "Action" || type === "Trigger") {
-                  if (name) {
-                    opt.push([name, uuid]);
-                  } else {
-                    opt.push([uuid, uuid]);
-                  }
+                // if (type === "Action" || type === "Trigger") {
+                //   if (name) {
+                //     opt.push([name, uuid]);
+                //   } else {
+                //     opt.push([uuid, uuid]);
+                //   }
+                // }
+                if (name) {
+                  opt.push([name, uuid]);
+                } else {
+                  opt.push([uuid, uuid]);
                 }
               });
             }
